63a51eda62d676154c8b063318594e6ed2926fa3
Martin Wiegand <mwiegand@seibert-media.net>
2016-10-12 23:34:02 +0200

    ui_play

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,10 +1,6 @@
 class MonthsController < ApplicationController
   before_action :set_month, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @months = Month.all
-  end
-
   def show
     @month_names = Month.names
     @month_duties = []
@@ -12,39 +8,6 @@ class MonthsController < ApplicationController
       @month_duties << employee.month_duties.find_or_create_by(
         date: @month
       )
-    end
-  end
-
-  def new
-    @month = Month.new
-  end
-
-  def edit
-  end
-
-  def create
-    @month = Month.new(month_params)
-
-    respond_to do |format|
-      if @month.save
-        format.html { redirect_to @month, notice: 'Month was successfully created.' }
-        format.json { render :show, status: :created, location: @month }
-      else
-        format.html { render :new }
-        format.json { render json: @month.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @month.update(month_params)
-        format.html { redirect_to @month, notice: 'Month was successfully updated.' }
-        format.json { render :show, status: :ok, location: @month }
-      else
-        format.html { render :edit }
-        format.json { render json: @month.errors, status: :unprocessable_entity }
-      end
     end
   end
 
