18edf679c917b845f33adebb697239903df44e11
Martin Wiegand <mwiegand@seibert-media.net>
2016-10-13 19:40:22 +0200

    score division by zero error fixed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,11 @@ class Employee < ApplicationRecord
     score = 0
     Month.where("date <= ?", Date.today).each do |month|
       score += fullfillments.where(month: month).count
-      score -= Bed.all.count * month_duties.find_by(month: month).count / month.month_duties.sum(:count)
+      if month.month_duties.sum(:count) != 0
+        score -= Bed.all.count * month_duties.find_by(month: month).count / month.month_duties.sum(:count)
+      else
+        0
+      end
     end
     score
   end
